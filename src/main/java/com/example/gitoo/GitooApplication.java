4ed187d4d1b839777eaaeacd4d329fd1178411ba
snowtaro <ask0208ask@gmail.com>
2026-01-03 16:07:47 +0900

    initial commit

---
+++ src/main/java/com/example/gitoo/GitooApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitooApplication.class, args);
+	}
+
+}
